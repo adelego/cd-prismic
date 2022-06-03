49ad83f9fc19b7b65b1dcf9e9ae1fafe910b4947
Adele Gauvrit <gauvrit.adele@gmail.com>
2022-06-03 10:42:34 +0200

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="text-slate-700">
+    <Header
+      v-if="navigation.data && settings.data"
+      :with-profile="withHeaderProfile"
+      :with-divider="withHeaderDivider"
+      :navigation="navigation"
+      :settings="settings"
+    />
+    <main><nuxt /></main>
+    <Footer v-if="navigation.data && settings.data" :with-sign-up-form="withFooterSignUpForm" :settings="settings" />
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    withHeaderProfile () {
+      return this.$store.state.layout.withHeaderProfile
+    },
+    withHeaderDivider () {
+      return this.$store.state.layout.withHeaderDivider
+    },
+    withFooterSignUpForm () {
+      return this.$store.state.layout.withFooterSignUpForm
+    },
+    navigation () {
+      return this.$store.state.prismic.navigation
+    },
+    settings () {
+      return this.$store.state.prismic.settings
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  @apply overflow-x-hidden antialiased;
+}
+</style>
